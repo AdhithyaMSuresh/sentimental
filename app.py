881ea8a270511481949ee80b7484d42e24fbcb39
AdhithyaMSuresh <163387023+AdhithyaMSuresh@users.noreply.github.com>
2024-05-01 22:17:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import streamlit as st
 import joblib
 
-#load the trained model
-model=joblib.load("sentiment-model.pkl")
+#Load the trained model
+model = joblib.load("sentiment-model.pkl")
 
-#define sentiment labels
-sentiment_labels={'1':'Positive','0':'Negative'}
+#Define sentiment labels
+sentiment_labels = {'1':'Positive', '0':'Negative'}
 
-#create streamlit app
+#Creaate Streamlit app
 st.title("Sentiment Analysis")
 
-#input text area
-user_input=st.text_area("Enter your text here:")
+#Input text area
+user_input = st.text_area("Enter your text here:")
 
-#prediction button
+#Prediction button
 if st.button("Predict"):
-    #perform sentiment prediction
-    predicted_sentiment=model.predict([user_input])[0]
-    predicted_sentiment_label=sentiment_labels[str(predicted_sentiment)]
+    #Perform sentiment prediction
+    print(user_input)
+    predicted_sentiment = model.predict([user_input])[0]
+    print("Predicted Label:" + str(predicted_sentiment))
+    predicted_sentiment_label =sentiment_labels[str(predicted_sentiment)]
 
-    #display predicted sentiment
-    st.info(f"Predicted Sentiment:{predicted_sentiment_label}")
+    #Display predicted sentiment
+    st.info(f"Predicted Sentiment: {predicted_sentiment_label}")
